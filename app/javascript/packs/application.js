88ad832d8881c4932b1d546a74d81091c8ac0b6c
oshimataishu <oshima.taishu.infratop@gmail.com>
2023-01-29 07:15:07 +0000

    　[Finish] project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery"
+import "script.js"
+
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
